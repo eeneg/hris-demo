716dd137f9d631b32e48f1d82d3b9461c376c6fd
John Reymar <pleasenotifyme@yahoo.com>
2021-12-03 13:02:12 +0800

    NOSI NOSA

--- resources/views/reports/nosa.blade.php
+++ resources/views/reports/nosa.blade.php
@@ -11,23 +11,107 @@
             .text-center {
                 text-align: center !important;
             }
-            .amounts {
-                font-family: Arial, Helvetica, sans-serif;
-                font-size: 80%;
-            }
             .center {
                 margin-left: 20px !important;
             }
-
-            table td{
-                margin: 0px 20px !important;
+            .h3{
+                margin-bottom: 0px;
             }
         </style>
 
     </head>
-    <body>
-        <div class="container-fluid center" style="" >
-            <h1>NOSA</h1>
-        </div>
+    <body style="width: 100%">
+        <main role="main" style="width: 100%">
+            <div style="width: 100%">
+                <div>
+                    <div class="col-sm-12 text-center">
+                        <h3 class="h3">PROVINCE OF DAVAO DEL SUR</h3>
+                        <h4 style="margin-bottom: 0px;margin-top: 5px;">Matti, Digos City</h4>
+                        <h3 style="margin-bottom: 0px;margin-top: 5px;">OFFICE OF THE GOVERNOR</h3>
+                        <h3 style=";margin-top: 5px;">NOTICE OF SALARY ADJUSTMENT</h3>
+                    </div>
+                </div>
+                <div class="col-sm-12">
+                    <div class="col-sm-12">
+                        <p class="h3" style="text-align: right; font-size: 18px;"><u>December 21, 2020</u></p>
+                    </div>
+                </div>
+                <div class="col-sm-12">
+                    <div class="col-sm-12">
+                        <p style="font-size: 18px; margin-bottom: 0px;">Christopher T. Tan</p>
+                        <p style="font-size: 18px; margin-top: 0px;  margin-bottom: 0px;">PGDH-Provincial Disaster Risk Reduction and Management Officer</p>
+                        <p style="font-size: 18px; margin-top: 0px;  margin-bottom: 0px;">Office of the Governor-Provincial Disaster Risk Reduction and Management Office</p>
+                        <p style="font-size: 18px; margin-top: 0px;">Province of Davao del Sur</p>
+                    </div>
+                </div>
+                <div class="col-sm-12">
+                    <div class="col-sm-12">
+                        <p style="font-size: 18px; margin-bottom: 10px;">Sir/Madam:</p>
+                        <p style="font-size: 18px; margin-top: 0px;  margin-bottom: 0px;"> &nbsp; &nbsp; &nbsp; &nbsp;Pursuant to Local Budget Circular No. <u><b>____</b></u> dated <b><u>_________, </u></b>implementing Republic Act No. 11466 dated January 8, 2020 your salary is hereby adjusted effective ____________ as follows.</p>
+                    </div>
+                </div>
+                <div class="col-sm-12" style="display: flex;" style="margin-top: 20px;">
+                    <div class="col-sm-6">
+                        <p style="font-size: 18px; margin-top: 20px;  margin-bottom: 3px;">1. Adjusted monthly basic salary effective </p>
+                        <p style="font-size: 18px; margin-top: 0px;  margin-bottom: 3px;">&nbsp; &nbsp; Under the New Salary Schedule: SG <u>26</u> Step <u>1</u>.</p>
+                    </div>
+                    <div class="col-sm-6" style="float: right; text-align: left;">
+                        <p style="font-size: 18px; margin-top: 20px;  margin-bottom: 3px;"><u><b>P&nbsp;69,936.00</b></u></p>
+                    </div>
+                </div>
+                <div class="col-sm-12" style="display: flex;" style="margin-top: 20px;">
+                    <div class="col-sm-6">
+                        <p style="font-size: 18px; margin-top: 20px;  margin-bottom: 3px;">2. Actual monthly basic salary as of </p>
+                        <p style="font-size: 18px; margin-top: 0px;  margin-bottom: 3px;">&nbsp; &nbsp; SG <u>26</u> Step <u>1</u>.</p>
+                    </div>
+                    <div class="col-sm-6" style="float: right; text-align: left;">
+                        <p style="font-size: 18px; margin-top: 20px;  margin-bottom: 3px;"><u><b>P&nbsp;69,936.00</b></u></p>
+                    </div>
+                </div>
+                <div class="col-sm-12" style="display: flex;" style="margin-top: 20px;">
+                    <div class="col-sm-6">
+                        <p style="font-size: 18px; margin-top: 20px;  margin-bottom: 3px;">3. Monthly salary adjustment effective_______ (1-2).</p>
+
+                    </div>
+                    <div class="col-sm-6" style="float: right; text-align: left;">
+                        <p style="font-size: 18px; margin-top: 20px;  margin-bottom: 3px;"><u><b>P&nbsp;69,936.00</b></u></p>
+                    </div>
+                </div>
+                <div class="col-sm-12">
+                    <div class="col-sm-12" style="margin-top: 50px;">
+                        <p style="font-size: 18px; margin-top: 0px;  margin-bottom: 0px;"> &nbsp; &nbsp; &nbsp; &nbsp;It is understood that this salary adjustment is subject to usual accounting and auditing rules adn regulations, and to appropriate re-adjustment and refund if found not in order.</p>
+                    </div>
+                </div>
+                <div class="col-sm-12">
+                    <div class="col-sm-12" style="margin-top: 50px;">
+                        <p style="font-size: 18px; margin-top: 0px;  margin-bottom: 0px; text-align: center;"> &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp;Very truly yours,</p>
+                    </div>
+                </div>
+                <div class="col-sm-12">
+                    <div class="col-sm-12" style="margin-top: 60px;">
+                        <p style="font-size: 18px; margin-top: 0px;  margin-bottom: 0px; text-align: right;"><b>MARC DOUGLAS IV C. CAGAS</b> </p>
+                    </div>
+                </div>
+                <div class="col-sm-12">
+                    <div class="col-sm-12" style="margin-top: 0px;">
+                        <p style="font-size: 18px; margin-top: 0px;  margin-bottom: 0px; text-align: right; margin-right: 50px;">Provincial Governor</p>
+                    </div>
+                </div>
+                <div class="col-sm-12">
+                    <div class="col-sm-12" style="margin-top: 30px;">
+                        <p style="font-size: 18px; margin-top: 0px;  margin-bottom: 0px;;">Position Title: <u>PGDH-Provincial Disaster Risk Reduction and Management Officer</u></p>
+                        <p style="font-size: 18px; margin-top: 0px;  margin-bottom: 0px;;">Salary Grade: <u>26</u></p>
+                        <p style="font-size: 18px; margin-top: 0px;  margin-bottom: 0px;;">Item No.: <u>62</u>, FY <u>2022</u> Plantilla of Personnel</p>
+                    </div>
+                </div>
+                <div class="col-sm-12">
+                    <div class="col-sm-12" style="margin-top: 20px;">
+                        <p style="font-size: 18px; margin-top: 0px;  margin-bottom: 0px;;">Copy Furnished: GSIS</p>
+
+                    </div>
+                </div>
+            </div>
+        </main>
+        <script type="text/php"></script>
     </body>
 </html>
--- resources/views/reports/nosi.blade.php
+++ resources/views/reports/nosi.blade.php
@@ -11,23 +11,116 @@
             .text-center {
                 text-align: center !important;
             }
-            .amounts {
-                font-family: Arial, Helvetica, sans-serif;
-                font-size: 80%;
-            }
             .center {
                 margin-left: 20px !important;
             }
-
-            table td{
-                margin: 0px 20px !important;
+            .h3{
+                margin-bottom: 0px;
             }
         </style>
 
     </head>
-    <body>
-        <div class="container-fluid center" style="" >
-            <h1>NOSI</h1>
-        </div>
+    <body style="width: 100%">
+        <main role="main" style="width: 100%">
+            <div style="width: 100%">
+                <div>
+                    <div class="col-sm-12 text-center">
+                        <h3 class="h3">PROVINCE OF DAVAO DEL SUR</h3>
+                        <h4 style="margin-bottom: 0px;margin-top: 5px;">Matti, Digos City</h4>
+                        <h3 style="margin-bottom: 0px;margin-top: 5px;">OFFICE OF THE GOVERNOR</h3>
+                        <h3 style=";margin-top: 5px;">NOTICE OF STEP INCREMENT</h3>
+                    </div>
+                </div>
+                <div class="col-sm-12">
+                    <div class="col-sm-12">
+                        <p class="h3" style="text-align: right; font-size: 18px;"><u>December 21, 2020</u></p>
+                    </div>
+                </div>
+                <div class="col-sm-12">
+                    <div class="col-sm-12">
+                        <p style="font-size: 18px; margin-bottom: 0px;">Christopher T. Tan</p>
+                        <p style="font-size: 18px; margin-top: 0px;  margin-bottom: 0px;">PGDH-Provincial Disaster Risk Reduction and Management Officer</p>
+                        <p style="font-size: 18px; margin-top: 0px;  margin-bottom: 0px;">Office of the Governor-Provincial Disaster Risk Reduction and Management Office</p>
+                        <p style="font-size: 18px; margin-top: 0px;">Province of Davao del Sur</p>
+                    </div>
+                </div>
+                <div class="col-sm-12">
+                    <div class="col-sm-12">
+                        <p style="font-size: 18px; margin-bottom: 10px;">Sir/Madam:</p>
+                        <p style="font-size: 18px; margin-top: 0px;  margin-bottom: 0px;"> &nbsp; &nbsp; &nbsp; &nbsp;Pursuant to to Joint Civil Service Commission (CSC) and Department of Budget and Management (DBM) Circular No. 1, s. 1990 implementing Section 13 &copy; of RA No. 6765, your Salary as <u><b> PGDH-Provincial Disaster Risk Reduction and Management Officer SG-22-1</b></u> is hereby adjusted effective <u><b>December 05, 2021.</b></u></p>
+                    </div>
+                </div>
+                <div class="col-sm-12">
+                    <div class="col-sm-12">
+                        <p style="font-size: 18px; margin-bottom: 3px;"> &nbsp; &nbsp; &nbsp; &nbsp;Basic Monthly Salary</p>
+                    </div>
+                </div>
+                <div class="col-sm-12" style="display: flex;">
+                    <div class="col-sm-6">
+                        <p style="font-size: 18px; margin-top: 0px;  margin-bottom: 3px;"> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;As of</b></u> <u><b>December 05, 2021.</b></u></p>
+                    </div>
+                    <div class="col-sm-6" style="float: right; text-align: left;">
+                        <p style="font-size: 18px; margin-top: 0px;  margin-bottom: 3px;"><u><b>P&nbsp;69,936.00</b></u></p>
+                    </div>
+                </div>
+                <div class="col-sm-12">
+                    <div class="col-sm-12">
+                        <p style="font-size: 18px; margin-top: 0px;  margin-bottom: 3px;"> &nbsp; &nbsp; &nbsp; &nbsp;Salary Adjustment</p>
+                    </div>
+                </div>
+                <div class="col-sm-12" style="display: flex;">
+                    <div class="col-sm-6">
+                        <p style="font-size: 18px; margin-top: 0px;  margin-bottom: 3px;"> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;a. Merit( <b><u>1</u></b> step/s )</p>
+                    </div>
+                    <div class="col-sm-6" style="float: right; text-align: left;">
+                        <p style="font-size: 18px; margin-top: 0px;  margin-bottom: 3px;"><u><b>P&nbsp;69,936.00</b></u></p>
+                    </div>
+                </div>
+                <div class="col-sm-12" style="display: flex;">
+                    <div class="col-sm-6">
+                        <p style="font-size: 18px; margin-top: 0px;  margin-bottom: 3px;"> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;a. Length of Service <b><u>1</u></b> step/s</p>
+                    </div>
+                    <div class="col-sm-6" style="float: right; text-align: left;">
+                        <p style="font-size: 18px; margin-top: 0px;  margin-bottom: 3px;"><u><b>P&nbsp;69,936.00</b></u></p>
+                    </div>
+                </div>
+                <div class="col-sm-12" style="display: flex;" style="margin-bottom: 20px;">
+                    <div class="col-sm-6">
+                        <p style="font-size: 18px; margin-top: 0px;  margin-bottom: 3px;"> &nbsp; &nbsp; &nbsp; &nbsp;Adjusted Salary Effective <u><b>December 05, 2022</b></u></p>
+                    </div>
+                    <div class="col-sm-6" style="float: right; text-align: left;">
+                        <p style="font-size: 18px; margin-top: 0px;  margin-bottom: 10px;"><u><b>P&nbsp;69,936.00</b></u></p>
+                    </div>
+                </div>
+                <div class="col-sm-12">
+                    <div class="col-sm-12" style="margin-top: 50px;">
+                        <p style="font-size: 18px; margin-top: 0px;  margin-bottom: 0px;"> &nbsp; &nbsp; &nbsp; &nbsp;The step increment/s is/are subject to review and Post Audit by the Department of Budget and Management and Subject to re-adjustment and refund if found not in order.</p>
+                    </div>
+                </div>
+                <div class="col-sm-12">
+                    <div class="col-sm-12" style="margin-top: 70px;">
+                        <p style="font-size: 18px; margin-top: 0px;  margin-bottom: 0px; text-align: center;"> &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp;Very truly yours,</p>
+                    </div>
+                </div>
+                <div class="col-sm-12">
+                    <div class="col-sm-12" style="margin-top: 70px;">
+                        <p style="font-size: 18px; margin-top: 0px;  margin-bottom: 0px; text-align: right;"><b>MARC DOUGLAS IV C. CAGAS</b> </p>
+                    </div>
+                </div>
+                <div class="col-sm-12">
+                    <div class="col-sm-12" style="margin-top: 0px;">
+                        <p style="font-size: 18px; margin-top: 0px;  margin-bottom: 0px; text-align: right; margin-right: 50px;">Provincial Governor</p>
+                    </div>
+                </div>
+                <div class="col-sm-12">
+                    <div class="col-sm-12" style="margin-top: 50px;">
+                        <p style="font-size: 18px; margin-top: 0px;  margin-bottom: 0px;;">Copy Furnished</p>
+                        <p style="font-size: 18px; margin-top: 0px;  margin-bottom: 0px;;">-GSIS</p>
+                        <p style="font-size: 18px; margin-top: 0px;  margin-bottom: 0px;;">-Office Concerned</p>
+                    </div>
+                </div>
+            </div>
+        </main>
+        <script type="text/php"></script>
     </body>
 </html>
