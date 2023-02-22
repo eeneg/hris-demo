3bd6b17ac18b5cc99669fd399b975ba7d8b9fbb0
Joowd <joowdx@pm.me>
2023-02-22 09:51:32 +0800

    Fix activities not being audited

--- app/Activity.php
+++ app/Activity.php
@@ -3,10 +3,9 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use Webpatser\Uuid\Uuid;
 
-class Activity extends Model
+class Activity extends Auditable
 {
     use HasFactory;
 
