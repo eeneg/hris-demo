ce8940953195ee9930a5ce3fdac73322ceec655e
eeneg <generellanos@gmail.com>
2024-05-21 11:56:47 +0800

    saln

--- resources/js/app.js
+++ resources/js/app.js
@@ -88,10 +88,10 @@ const routes = [
     { path: '/employees', component: require('./components/Employees.vue').default },
     { path: '/employees-pds', component: require('./components/employees/Pds-form.vue').default },
     { path: '/employees-pds-view', component: require('./components/employees/EmployeePDS.vue').default },
+    { path: '/employees-saln', name: 'saln', component: require('./components/employees/EmployeeSALN.vue').default },
     { path: '/employee-pds-edit-requests', component: require('./components/employees/EmployeeEditRequests.vue').default },
     { path: '/employee-reappointments', component: require('./components/employees/Reappointments.vue').default },
     { path: '/employee-status', component: require('./components/employees/EmployeeStatus.vue').default },
-    { path: '/employee-SALN', component: require('./components/employees/EmployeeSALN.vue').default },
     { path: '/employee-leave-applications', component: require('./components/employees/EmployeeLeaveApplications.vue').default },
     { path: '/employee-leave-form', component: require('./components/employees/EmployeeLeaveForm.vue').default },
     { path: '/employee-service-record', component: require('./components/servicerecords/ServiceRecords.vue').default },
@@ -115,8 +115,8 @@ const routes = [
 ]
 
 const router = new VueRouter({
+    routes, // short for `routes: routes`
     mode: 'history',
-    routes // short for `routes: routes`
 })
 
 const toast = Swal.mixin({
