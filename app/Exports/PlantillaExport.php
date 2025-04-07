648a42d283b50b9ddb0b4ff28a5efd16be4de5b2
edwinrojo15@gmail.com <edwinrojo15@gmail.com>
2025-04-07 13:14:51 +0800

    excel report

--- app/Exports/PlantillaExport.php
+++ app/Exports/PlantillaExport.php
@@ -5,8 +5,10 @@ namespace App\Exports;
 use App\PlantillaContent;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithStyles;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 
-class PlantillaExport implements FromCollection, WithHeadings
+class PlantillaExport implements FromCollection, WithHeadings, WithStyles
 {
 
     /**
@@ -18,6 +20,46 @@ class PlantillaExport implements FromCollection, WithHeadings
     public function __construct($plantilla_id)
     {
         $this->plantilla_id = $plantilla_id;
+    }
+
+    public function styles(Worksheet $sheet)
+    {
+        $sheet->getStyle('A1:AE2')->getFont()->setBold(true);
+        $sheet->getStyle('A1:AE2')->getAlignment()->setHorizontal('center');
+        $sheet->getStyle('A1:AE2')->getAlignment()->setVertical('center');
+        $sheet->getStyle('A1:AE2')->getBorders()->getAllBorders()->setBorderStyle(\PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN);
+        $sheet->getStyle('A1:AE2')->getFill()->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID);
+        $sheet->getStyle('A1:AE2')->getFill()->getStartColor()->setARGB('FFCCFFCC');
+        $sheet->getStyle('A1:AE2')->getFont()->setSize(12);
+
+        // Merge cells for the header
+        $sheet->mergeCells('A1:A2', Worksheet::MERGE_CELL_CONTENT_MERGE);
+        $sheet->mergeCells('B1:B2', Worksheet::MERGE_CELL_CONTENT_MERGE);
+        $sheet->mergeCells('C1:C2', Worksheet::MERGE_CELL_CONTENT_MERGE);
+        $sheet->mergeCells('D1:D2', Worksheet::MERGE_CELL_CONTENT_MERGE);
+        $sheet->mergeCells('E1:E2', Worksheet::MERGE_CELL_CONTENT_MERGE);
+        $sheet->mergeCells('F1:F2', Worksheet::MERGE_CELL_CONTENT_MERGE);
+        $sheet->mergeCells('G1:G2', Worksheet::MERGE_CELL_CONTENT_MERGE);
+        $sheet->mergeCells('H1:H2', Worksheet::MERGE_CELL_CONTENT_MERGE);
+        $sheet->mergeCells('I1:I2', Worksheet::MERGE_CELL_CONTENT_MERGE);
+        $sheet->mergeCells('J1:J2', Worksheet::MERGE_CELL_CONTENT_MERGE);
+        $sheet->mergeCells('K1:K2', Worksheet::MERGE_CELL_CONTENT_MERGE);
+        $sheet->mergeCells('L1:L2', Worksheet::MERGE_CELL_CONTENT_MERGE);
+        $sheet->mergeCells('M1:M2', Worksheet::MERGE_CELL_CONTENT_MERGE);
+        $sheet->mergeCells('N1:N2', Worksheet::MERGE_CELL_CONTENT_MERGE);
+        $sheet->mergeCells('O1:O2', Worksheet::MERGE_CELL_CONTENT_MERGE);
+        $sheet->mergeCells('P1:P2', Worksheet::MERGE_CELL_CONTENT_MERGE);
+        $sheet->mergeCells('Q1:S1', Worksheet::MERGE_CELL_CONTENT_MERGE);
+        $sheet->mergeCells('T1:V1', Worksheet::MERGE_CELL_CONTENT_MERGE);
+        $sheet->mergeCells('W1:W2', Worksheet::MERGE_CELL_CONTENT_MERGE);
+        $sheet->mergeCells('X1:X2', Worksheet::MERGE_CELL_CONTENT_MERGE);
+        $sheet->mergeCells('Y1:Y2', Worksheet::MERGE_CELL_CONTENT_MERGE);
+        $sheet->mergeCells('Z1:Z2', Worksheet::MERGE_CELL_CONTENT_MERGE);
+        $sheet->mergeCells('AA1:AA2', Worksheet::MERGE_CELL_CONTENT_MERGE);
+        $sheet->mergeCells('AB1:AB2', Worksheet::MERGE_CELL_CONTENT_MERGE);
+        $sheet->mergeCells('AC1:AC2', Worksheet::MERGE_CELL_CONTENT_MERGE);
+        $sheet->mergeCells('AD1:AD2', Worksheet::MERGE_CELL_CONTENT_MERGE);
+        $sheet->mergeCells('AE1:AE2', Worksheet::MERGE_CELL_CONTENT_MERGE);
     }
 
     public function collection()
@@ -34,7 +76,7 @@ class PlantillaExport implements FromCollection, WithHeadings
             ->map(function($item){
                 return [
                     'department' => $item->position->department->address,
-                    'old_number' => $item->old_number,  
+                    'old_number' => $item->old_number,
                     'new_number' => $item->new_number,
                     'position_title' => $item->position->title,
                     'last_name' => $item->personalinformation ? $item->personalinformation->surname : '',
@@ -45,11 +87,16 @@ class PlantillaExport implements FromCollection, WithHeadings
                     'tin' => $item->personalinformation ? $item->personalinformation->tin : '',
                     'level' => $item->level,
                     'first_eligibility' => $item->personalinformation ? (count($item->personalinformation->eligibilities) > 0 ? $item->personalinformation->eligibilities[0]->careerService : '') : '',
-                    'regligion' => $item->personalinformation ? $item->personalinformation->religion : '',
+                    'regligion' => $item->personalinformation ? ($item->personalinformation->religion == 'Others' ? $item->personalinformation->other_religion : $item->personalinformation->religion) : '',
+                    'indigenous_people' => $item->personalinformation ? ($item->personalinformation->indigenous_people == 'Others' ? $item->personalinformation->other_indigenous_people : $item->personalinformation->indigenous_people) : '',
+                    'disability' => $item->personalinformation ? ($item->personalinformation->disability == 'Others' ? $item->personalinformation->other_disability : $item->personalinformation->disability) : '',
+                    'solo_parent_number' => $item->personalinformation ? $item->personalinformation->solo_parent_number : '',
                     // 'employee' => $item->personalinformation ? $item->personalinformation->surname.($item->personalinformation->nameextension ? ' '.$item->personalinformation->nameextension : '').', '.$item->personalinformation->firstname.' '.$item->personalinformation->middlename : 'VACANT',
-                    'current_year_salary' => $item->salaryauthorized ? $item->salaryauthorized->grade . '/' . $item->salaryauthorized->step : '',
+                    'current_year_grade' => $item->salaryauthorized ? $item->salaryauthorized->grade : '',
+                    'current_year_step' => $item->salaryauthorized ? $item->salaryauthorized->step : '',
                     'current_year_salary_amount' => $item->salaryauthorized ? $item->salaryauthorized->amount : '',
-                    'budget_year_salary' => $item->salaryproposed ? $item->salaryproposed->grade . '/' . $item->salaryproposed->step : '',
+                    'budget_year_grade' => $item->salaryproposed ? $item->salaryproposed->grade : '',
+                    'budget_year_step' => $item->salaryproposed ? $item->salaryproposed->step : '',
                     'budget_year_salary_amount' => $item->salaryproposed ? $item->salaryproposed->amount : '',
                     'date_of_birth' => $item->personalinformation ? date("m/d/Y", strtotime($item->personalinformation->birthdate)) : '',
                     'date_of_original_appointment' => date("m/d/Y", strtotime($item->original_appointment)),
@@ -82,9 +129,14 @@ class PlantillaExport implements FromCollection, WithHeadings
             'Level',
             'Eligibility',
             'Religion',
+            'Indigenous People',
+            'Disability',
+            'Solo Parent Number',
             'Current Year Authorized',
             '',
+            '',
             'Budget Year Proposed',
+            '',
             '',
             'Date of Birth',
             'Date of Original Appointment',
@@ -111,9 +163,14 @@ class PlantillaExport implements FromCollection, WithHeadings
             '',
             '',
             '',
-            'Grade/Step',
+            '',
+            '',
+            '',
+            'Grade',
+            'Step',
             'Amount',
-            'Grade/Step',
+            'Grade',
+            'Step',
             'Amount',
             '',
             '',
